4929001a9243fc48f2321dc49ad2fcbc023f6594
Surya Kosaraju <mourya.kosaraju@gmail.com>
2019-05-08 15:17:43 +0400

    Initial commit
    Signed-off-by: Surya Kosaraju <mourya.kosaraju@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//        <img src={logo} className="App-logo" alt="logo" />
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      articles: [ ],
+      isLoading: true,
+      sourcen: null,
+    }
+this.componentDidMount = this.componentDidMount.bind(this);
+  }
+ // async handleSourceChange(url){
+
+ //    const response2 = fetch(url2);
+ //    const datas = response2.json();
+ //    this.setState({articles: datas.articles, isLoading: false});
+ //    console.log(datas.articles);
+ //    this.render();
+ //  }
+ 
+  async componentDidMount(){
+
+    // await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=90f3115a267341bca2b9fe69d0653820')
+    //   .then(res => res.json())
+    //   .then(json => {
+    //     this.setState({
+    //       isLoaded: true,
+    //       items: json,
+    //     })
+    //   });
+    // if(sourcen==null){
+
+          const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=90f3115a267341bca2b9fe69d0653820';
+      const response = await fetch(url);
+      const data = await response.json();
+      this.setState({articles: data.articles, isLoading: false });
+      console.log(data.articles);
+
+    // }else{
+
+    //           const url = 'https://newsapi.org/v2/everything?domains=${sourcen}&apiKey=90f3115a267341bca2b9fe69d0653820'
+    //                 const response = await fetch(url);
+    //                       const data = await response.json();
+    //         this.setState({articles: data.articles, isLoading: false });
+    //               console.log(data.articles);
+
+    
+  // }
+}
+ 
+
+  render(){
+
+        return (
+          <div className="App">
+          {this.state.isLoading || !this.state.articles ? (
+            <p> Loading...</p>
+            ): ( 
+
+            //New method of displaying news ina tabular manner. 
+            <div>
+            <img width="100px" height="100px" src="https://media.featuredcustomers.com/Company.logo/Chalhoub_Group.png"></img>
+            <br></br>
+            <br></br>
+             <h1 id="maintitle">Chalhoub's Freshly Squeezed News!</h1>
+<br></br><br></br>
+             <div class="page-wrap">                
+
+             <ul>
+             {this.state.articles.map(article =>(
+              <li class="main-content">
+                <h1 class="titlee">{article.title}</h1>              
+                  <img width="100%" height="100%" src={article.urlToImage}></img>
+
+                <p class="desc"><strong>{article.description}</strong></p>
+                <p class="cont">{article.content} <a href={article.url}>For more</a></p>
+                              <br></br>
+
+              </li> 
+              ))}
+             </ul>
+              <br></br>
+
+              <nav class="main-nav">
+                <h2>Source Category</h2>
+                <ul>
+                {this.state.articles.map(article =>(
+                  <li class="source-list"><br></br><a class="source-list" href="#">{article.source.name}</a><br></br>----------</li>
+                  ))}
+                </ul>
+              </nav>
+              
+            </div>
+</div>
+        //Old method
+            // <div>
+            //  <h3>Freshly brewed News!</h3>
+            //   <ul>
+            //      {this.state.articles.map(article =>(
+
+            //          <li key={article.source.key}>
+            //          Name: {article.title}
+            //          <br></br>
+            //         <a href={article.url}> <img width="200px" height="150px" src={article.urlToImage}></img></a><br></br>
+            //          Source: {article.source.name}
+            //                              <br></br>
+            //          <br></br>
+
+            //          </li>
+            //        ))}
+            //    </ul>   
+            //                <p align="left">Chalhoub Front-End Engineer Intern Test --- Surya Kosaraju -- Genius Poor Non-philanthropic Goodguy</p>
+            // </div>
+
+            )}
+
+          </div>
+
+        );
+   
+  }
+}
+
+export default App;
